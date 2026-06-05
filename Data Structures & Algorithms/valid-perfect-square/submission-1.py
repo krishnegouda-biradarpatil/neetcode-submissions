9e569795afd5e85b256b7100fbed09f817c37775
krishnegouda-biradarpatil <krishnegoudabiradarpatil@gmail.com>
2026-06-05 14:02:20 +0530

    Add: valid-perfect-square - submission-1

---
+++ Data Structures & Algorithms/valid-perfect-square/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if num==1:
+            return True
+        for i in range(2,num//2+1):
+            if i*i==num:
+                return True
+        return False
